306682761339f35fd5526f26134a25b6571e15ed
MuskanMangla23 <muskanmangla.com>
2025-07-06 14:39:12 +0530

    Add FeedBack component with Marquee for testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import HeroSection from "./components/HeroSection"
+import HeroSection from "./components/HeroSection";
+import FeedBack from "./components/FeedBack";
 
 function App() {
   return (
     <div>
       <HeroSection />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/FeedBack.jsx
@@ -0,0 +1,107 @@
+import Marquee from 'react-fast-marquee' ;
+import person1 from '../assets/influencer_01.jpg'
+export default function FeedBack() {
+  return (
+    <div className="w-full min-h-screen flex flex-col justify-center items-center bg-[#181818] text-neutral-200 ">
+      <div className="flex flex-col">
+        <div className="flex space-y-2 flex-col mb-14 mx-5 mx-20">
+            <Marquee
+                pauseOnHover
+                gradient
+                gradientColor='rgb(22,22,22)'
+                gradientWidth={150} 
+                className='my-2'
+            >
+                <div className='bg-[#202020] m-1 max-w-sm rounded-xl flex p-2 font-medium cursor-pointer text-white hover:scale-105 mx-2'>
+                    <img src={person1} alt="" className="h-[150px] w-[100px] object-cover rounded-md" />
+                    <div className='rounded-md flex flex-col justify-between my-2 ml-2'>
+                        <div className='text-base text-gray-300'>“Joining this gym changed my life! I lost 10 kg in 3 months and feel more confident and energized every day.”</div>
+                        <div className='text-neutral-500 font-normal m'>- Priya Mehta</div>
+                    </div>
+                </div>
+                <div className='bg-[#202020] m-1 max-w-sm rounded-xl flex p-2 font-medium cursor-pointer text-white hover:scale-105 mx-2'>
+                    <img src={person1} alt="" className="h-[150px] w-[100px] object-cover rounded-md" />
+                    <div className='rounded-md flex flex-col justify-between my-2 ml-2'>
+                        <div className='text-base text-gray-300'>“Joining this gym changed my life! I lost 10 kg in 3 months and feel more confident and energized every day.”</div>
+                        <div className='text-neutral-500 font-normal m'>- Priya Mehta</div>
+                    </div>
+                </div>                
+                <div className='bg-[#202020] m-1 max-w-sm rounded-xl flex p-2 font-medium cursor-pointer text-white hover:scale-105 mx-2'>
+                    <img src={person1} alt="" className="h-[150px] w-[100px] object-cover rounded-md" />
+                    <div className='rounded-md flex flex-col justify-between my-2 ml-2'>
+                        <div className='text-base text-gray-300'>“Joining this gym changed my life! I lost 10 kg in 3 months and feel more confident and energized every day.”</div>
+                        <div className='text-neutral-500 font-normal m'>- Priya Mehta</div>
+                    </div>
+                </div>                
+                <div className='bg-[#202020] m-1 max-w-sm rounded-xl flex p-2 font-medium cursor-pointer text-white hover:scale-105 mx-2'>
+                    <img src={person1} alt="" className="h-[150px] w-[100px] object-cover rounded-md" />
+                    <div className='rounded-md flex flex-col justify-between my-2 ml-2'>
+                        <div className='text-base text-gray-300'>“Joining this gym changed my life! I lost 10 kg in 3 months and feel more confident and energized every day.”</div>
+                        <div className='text-neutral-500 font-normal m'>- Priya Mehta</div>
+                    </div>
+                </div>
+                <div className='bg-[#202020] m-1 max-w-sm rounded-xl flex p-2 font-medium cursor-pointer text-white hover:scale-105 mx-2'>
+                    <img src={person1} alt="" className="h-[150px] w-[100px] object-cover rounded-md" />
+                    <div className='rounded-md flex flex-col justify-between my-2 ml-2'>
+                        <div className='text-base text-gray-300'>“Joining this gym changed my life! I lost 10 kg in 3 months and feel more confident and energized every day.”</div>
+                        <div className='text-neutral-500 font-normal m'>- Priya Mehta</div>
+                    </div>
+                </div>                
+                <div className='bg-[#202020] m-1 max-w-sm rounded-xl flex p-2 font-medium cursor-pointer text-white hover:scale-105 mx-2'>
+                    <img src={person1} alt="" className="h-[150px] w-[100px] object-cover rounded-md" />
+                    <div className='rounded-md flex flex-col justify-between my-2 ml-2'>
+                        <div className='text-base text-gray-300'>“Joining this gym changed my life! I lost 10 kg in 3 months and feel more confident and energized every day.”</div>
+                        <div className='text-neutral-500 font-normal m'>- Priya Mehta</div>
+                    </div>
+                </div>
+            </Marquee>
+
+            <Marquee
+                direction='right'
+                pauseOnHover
+            >
+                <div className='bg-[#202020] m-1 max-w-sm rounded-xl flex p-2 font-medium cursor-pointer text-white hover:scale-105 mx-2'>
+                    <img src={person1} alt="" className="h-[150px] w-[100px] object-cover rounded-md" />
+                    <div className='rounded-md flex flex-col justify-between my-2 ml-2'>
+                        <div className='text-base'>“Joining this gym changed my life! I lost 10 kg in 3 months and feel more confident and energized every day.”</div>
+                        <div>— Priya Mehta, College Student</div>
+                    </div>
+                </div>
+                                <div className='bg-[#202020] m-1 max-w-sm rounded-xl flex p-2 font-medium cursor-pointer text-white hover:scale-105 mx-2'>
+                    <img src={person1} alt="" className="h-[150px] w-[100px] object-cover rounded-md" />
+                    <div className='rounded-md flex flex-col justify-between my-2 ml-2'>
+                        <div className='text-base'>“Joining this gym changed my life! I lost 10 kg in 3 months and feel more confident and energized every day.”</div>
+                        <div>— Priya Mehta, College Student</div>
+                    </div>
+                </div>                <div className='bg-[#202020] m-1 max-w-sm rounded-xl flex p-2 font-medium cursor-pointer text-white hover:scale-105 mx-2'>
+                    <img src={person1} alt="" className="h-[150px] w-[100px] object-cover rounded-md" />
+                    <div className='rounded-md flex flex-col justify-between my-2 ml-2'>
+                        <div className='text-base'>“Joining this gym changed my life! I lost 10 kg in 3 months and feel more confident and energized every day.”</div>
+                        <div>— Priya Mehta, College Student</div>
+                    </div>
+                </div>                <div className='bg-[#202020] m-1 max-w-sm rounded-xl flex p-2 font-medium cursor-pointer text-white hover:scale-105 mx-2'>
+                    <img src={person1} alt="" className="h-[150px] w-[100px] object-cover rounded-md" />
+                    <div className='rounded-md flex flex-col justify-between my-2 ml-2'>
+                        <div className='text-base'>“Joining this gym changed my life! I lost 10 kg in 3 months and feel more confident and energized every day.”</div>
+                        <div>— Priya Mehta, College Student</div>
+                    </div>
+                </div>
+                                <div className='bg-[#202020] m-1 max-w-sm rounded-xl flex p-2 font-medium cursor-pointer text-white hover:scale-105 mx-2'>
+                    <img src={person1} alt="" className="h-[150px] w-[100px] object-cover rounded-md" />
+                    <div className='rounded-md flex flex-col justify-between my-2 ml-2'>
+                        <div className='text-base'>“Joining this gym changed my life! I lost 10 kg in 3 months and feel more confident and energized every day.”</div>
+                        <div>— Priya Mehta, College Student</div>
+                    </div>
+                </div>                <div className='bg-[#202020] m-1 max-w-sm rounded-xl flex p-2 font-medium cursor-pointer text-white hover:scale-105 mx-2'>
+                    <img src={person1} alt="" className="h-[150px] w-[100px] object-cover rounded-md" />
+                    <div className='rounded-md flex flex-col justify-between my-2 ml-2'>
+                        <div className='text-base'>“Joining this gym changed my life! I lost 10 kg in 3 months and feel more confident and energized every day.”</div>
+                        <div>— Priya Mehta, College Student</div>
+                    </div>
+                </div>
+            </Marquee>
+        </div>
+      </div>
+    </div>
+  );
+}
